87654a0725b7a818694bcb86c89207f815877f5e
julianne03 <julianneyi03@gmail.com>
2024-06-16 23:15:54 -0700

    navbar design chage with text align #1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,27 +5,29 @@ import '../styles/common/Navbar.css'
 const Nav = () => {
   return (
     <nav>
-      <div>
-        <NavLink to="/">
-          Julianne's Portfolio
-        </NavLink>
+      <div className="menu">
+        <div>
+            <NavLink to="/">
+            Julianne's Portfolio
+            </NavLink>
+        </div>
+        <div>
+            <NavLink to="/about">
+            About
+            </NavLink>
+        </div>
+        <div>
+            <NavLink to="/projects">
+            Projects
+            </NavLink>
+        </div>
+        <div>
+            <NavLink to="/blog">
+            Blog
+            </NavLink>
+        </div>
       </div>
-      <div>
-        <NavLink to="/about">
-          About
-        </NavLink>
-      </div>
-      <div>
-        <NavLink to="/projects">
-          Projects
-        </NavLink>
-      </div>
-      <div>
-        <NavLink to="/blog">
-          Blog
-        </NavLink>
-      </div>
-      <div>
+      <div className="contactBox">
         <NavLink to="/">
             <a className="contact" href="#contact">Contact</a>
         </NavLink>
